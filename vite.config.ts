b06013587094da4889aae701d9aa5d1f1666924f
Cruz Wootten <78571191+LilWikipedia@users.noreply.github.com>
2024-11-29 19:04:05 +0000

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/cruz-wootten"
+})
+
+
+
+
+
+{/* https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -11,6 +18,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    
     mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
@@ -19,3 +27,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+*/}
